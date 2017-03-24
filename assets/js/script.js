ec5362bf04b2c95350dc38b7b7f230805145e094
Michiel Leyman <michiel.leyman@gmail.com>
2017-03-24 01:10:03 +0000

    Some questions logic

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,72 @@
-const getQuestions = () => {
-  return [
-    'This is a silly question',
-    'This is another silly question, right?',
-    ''
+const clone = function (o) {
+  if (null == o || 'object' != typeof o) return o;
+  const copy = o.constructor();
+  for (var attr in o) {
+    if (o.hasOwnProperty(attr)) copy[attr] = o[attr];
+  }
+  return copy;
+}
+
+const Queries = (() => {
+  const predefined = [
+    {
+      question: 'This is a silly question',
+      response: 'This is a silly answer'
+    },
+    {
+      question: 'This is another silly question, right?',
+      response: 'Pretty much'
+    },
+    {
+      question: 'Hi I\'m a woman',
+      response: 'Hello woman.'
+    },
+    {
+      question: '',
+      response: ''
+    },
+    {
+      question: '',
+      response: ''
+    },
+    {
+      question: '',
+      response: ''
+    }
   ];
-};
+
+  const get = () => {
+    return clone(predefined);
+  };
+
+  const answerQuestion = (question) => {
+    return predefined.filter((query) => {
+      return query.question === question;
+    })[0].response;
+  };
+
+  return {
+    get: get,
+    getAnswerTo: answerQuestion
+  }
+})();
+
+const Alex = (() => {
+  const respond = (question) => {
+    $('body').append(Queries.getAnswerTo(question));
+  };
+  return { respond: respond };
+})();
 
 $(document).ready((function() {
-  getQuestions().forEach((question) => {
-    $option = $(`<option value="${question}">${question}</option>`);
+  const input = $('input[name=questionlist]');
+  Queries.get().forEach((query) => {
+    $option = $(`<option value="${query.question}">${query.question}</option>`);
     $('datalist').append($option);
+  });
+  input.on('keyup', (e) => {
+    if (e.keyCode !== 13) return;
+    e.preventDefault();
+    Alex.respond(input.val());
   });
 }));
